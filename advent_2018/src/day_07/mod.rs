d4300ce67ee5bbf27af4d651f7d2488a6de2e32f
Arthur E. Jones <PartyLich@gmail.com>
2021-11-11 17:06:34 -0600

    feat: parse instruction steps

--- advent_2018/src/day_07/mod.rs
+++ advent_2018/src/day_07/mod.rs
@@ -1,5 +1,24 @@
 //! Solutions to 2018 day 7 problems
 //! --- Day 7: The Sum of Its Parts ---
+use lazy_static::lazy_static;
+use regex::Regex;
+
+/// Parse a step and its precondition from str
+fn parse_line(input: &str) -> Result<(char, char), &'static str> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r#"Step (\w) must be finished before step (\w) can begin."#).unwrap();
+    }
+
+    RE.captures(input)
+        .map(|captures| {
+            let parent = captures.get(1).unwrap().as_str().chars().next().unwrap();
+            let child = captures.get(2).unwrap().as_str().chars().next().unwrap();
+            (parent, child)
+        })
+        .ok_or("Failed to parse rule")
+}
+
 
 /// returns the order of the assembly steps
 pub fn one(file_path: &str) -> String {
