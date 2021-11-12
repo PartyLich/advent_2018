c99d8e5680c17cfda5f1e7417935d69f1d5ef41e
Arthur E. Jones <PartyLich@gmail.com>
2021-11-11 20:26:53 -0600

    feat: convert char to step completion time

--- advent_2018/src/day_07/two.rs
+++ advent_2018/src/day_07/two.rs
@@ -1,6 +1,11 @@
 //! Solutions to 2018 day 7 problems part two
 //! --- Day 7: The Sum of Its Parts ---
 
+/// get the duration for a step with the supplied label
+fn get_time(ch: char) -> usize {
+    ch as usize - 64 + 60
+}
+
 /// returns the completion time (in seconds) of the assembly steps
 pub fn two(file_path: &str) -> usize {
     todo!();
@@ -9,6 +14,18 @@ pub fn two(file_path: &str) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn finds_time() {
+        let msg = "should return the duration for a given step label";
+        let expected = 61;
+        let actual = get_time('A');
+        assert_eq!(actual, expected, "{}", msg);
+
+        let expected = 86;
+        let actual = get_time('Z');
+        assert_eq!(actual, expected, "{}", msg);
+    }
 
     #[test]
     fn part_two() {
